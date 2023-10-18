afe54457069127996e8f88631e0ffde9a6c8072b
shaan.lee <dev.sehyeon@gmail.com>
2023-10-18 14:58:13 +0900

    add get_node method in manager class

--- src/neo4j_manager.py
+++ src/neo4j_manager.py
@@ -5,3 +5,31 @@ class Neo4jManager:
     def __init__(self, URI, USERNAME, PASSWORD):
         self.URI = URI
         self.AUTH = (USERNAME, PASSWORD)
+
+    def __execute_query(self, query):
+        with GraphDatabase.driver(self.URI, auth=self.AUTH) as driver:
+            result = driver.execute_query(query)
+        return result
+
+    def __convert_to_js_object_str(self, dict):
+        return_str = "{"
+        for key in dict:
+            if return_str != "{":
+                return_str += ","
+            return_str += key
+            return_str += ":"
+            if isinstance(dict[key], str):
+                return_str += f"'{dict[key]}'"
+            else:
+                return_str += f"'{dict[key]}'"
+        return_str += "}"
+        return return_str
+
+    def get_node(self, node_values):
+        node_values["values"] = self.__convert_to_js_object_str(
+            node_values.get("values")
+        )
+        results = self.__execute_query(
+            "Match (x:%(table)s %(values)s)" "Return x" % node_values
+        )
+        return results
